64223e9df95315f22ac0ecac3e8560a3c3275d82
KienTD2005 <kie2127421@maricopa.edu>
2024-04-30 17:02:09 -0700

    For Project 5

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -4,14 +4,47 @@ public class SortingUtility {
 
 
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
+        int position = 0;
+        while (position < data.length){
+            if (position == 0 || data[position].compareTo(data[position - 1]) >= 0) {
+                position = position + 1;
+            }
+            else {
+                swap(data, position, position - 1);
+                position = position - 1;
+            }
 
-        // TODO implement Gnome Sort here
+        }
     }
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
+        boolean swapped;
+        int start = 0;
+        int end = data.length - 2;
+        do
+        {
+            swapped = false;
+            // Forward pass (from left to right)
+            for (int i = start; i <= end; i++) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
 
-        // TODO implement Cocktail Shaker Sort here
+        // Backward pass (from right to left)
+            swapped = false;
+            for (int i = end; i >= start; i--) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+        }while (swapped);
     }
 
 
